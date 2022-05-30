aa911e98afbaf44f7d3859d2e345509c39b51cf7
murali0101 <muralimv0101@gmail.com>
2022-05-30 12:37:32 +0530

    Update TableRow.jsx

--- src/components/Table/TableRow.jsx
+++ src/components/Table/TableRow.jsx
@@ -2,8 +2,14 @@ import './TableRow.css'
 
 
 
-export const TableRow = () => {
+export const TableRow = ({ props }) => {
+    const {population,countryId:{country_name},city_name}=props.ele
     return <tr>
-        
+        <td>{props.ind+1 }</td>
+        <td>{country_name}</td>
+        <td>{city_name}</td>
+        <td>{population}</td>
+        <td>Edit</td>
+        <td>Delete</td>
     </tr>
 }
