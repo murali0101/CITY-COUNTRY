9fc00e05aedd194f414c7dd7addfa09ae911932c
murali0101 <muralimv0101@gmail.com>
2022-05-30 13:02:42 +0530

    Update AddCountry.jsx

--- src/components/AddCountry/AddCountry.jsx
+++ src/components/AddCountry/AddCountry.jsx
@@ -1,1 +1,33 @@
-import './AddCountry.css'
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { postCountry } from "../../redux/cityCountryReducer/CityCountryAction";
+import "./AddCountry.css";
+
+export const AddCountry = () => {
+  const dispatch = useDispatch();
+  const [country, setCountry] = useState({ country_name: "" });
+  const formHandler = (e) => {
+    e.preventDefault();
+    dispatch(postCountry(country));
+  };
+  const fieldHandler = (e) => {
+    const { name, value } = e.target;
+    setCountry({ ...country, [name]: value });
+  };
+  console.log(country);
+  return (
+    <div>
+      <form onSubmit={formHandler}>
+        <input
+          value={country.country_name}
+          type="text"
+          name="country_name"
+          placeholder="Please Enter Country Name"
+          onChange={fieldHandler}
+          required
+        />
+        <input type="submit" value="Add Country" />
+      </form>
+    </div>
+  );
+};
