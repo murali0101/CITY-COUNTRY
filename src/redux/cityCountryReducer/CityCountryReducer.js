6da958165f711c49229b241b00351001859163f2
murali0101 <muralimv0101@gmail.com>
2022-05-30 11:59:58 +0530

    Create CityCountryReducer.js

---
+++ src/redux/cityCountryReducer/CityCountryReducer.js
@@ -0,0 +1,10 @@
+const init = {
+  country: [],
+  city: [],
+};
+export const CityCountryReducer = (store = init, { type, payload }) => {
+  switch (type) {
+    default:
+      return store;
+  }
+};
