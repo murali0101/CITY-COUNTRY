027398e7993fe5ffd4c3bbe9214e797c65d7553d
murali0101 <muralimv0101@gmail.com>
2022-05-30 20:22:02 +0530

    Update CityCountryAction.js

--- src/redux/cityCountryReducer/CityCountryAction.js
+++ src/redux/cityCountryReducer/CityCountryAction.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 export const ADDDATA = "ADDDATA";
+export const ADDCOUNTRY = "ADDCOUNTRY";
 export const addData = (val) => {
   return { type: ADDDATA, payload: val };
+};
+export const addCountry = (val) => {
+  return { type: ADDCOUNTRY, payload: val };
 };
 export const getData = () => async (dispatch) => {
   axios
@@ -9,9 +13,25 @@ export const getData = () => async (dispatch) => {
     .then((res) => dispatch(addData(res.data)))
     .catch((error) => console.log(error));
 };
+export const getCountry = () => async (dispatch) => {
+  axios
+    .get("https://city-country-01.herokuapp.com/add-country/")
+    .then((res) => dispatch(addCountry(res.data)))
+    .catch((error) => console.log(error));
+};
 export const postCountry = (val) => async (dispatch) => {
   axios
-    .post("https://city-country-01.herokuapp.com/add-country/",{...val})
-    .then((res) => {console.log(res)} )
+    .post("https://city-country-01.herokuapp.com/add-country/", { ...val })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => console.log(error));
+};
+export const postcity= (val) => async (dispatch) => {
+  axios
+    .post("https://city-country-01.herokuapp.com/add-city/", { ...val })
+    .then((res) => {
+      dispatch(getData(res.data))
+    })
     .catch((error) => console.log(error));
 };
