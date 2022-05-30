0b3e122e11d8a9d8ccbae6e5b196ffe40c27e929
murali0101 <muralimv0101@gmail.com>
2022-05-30 20:41:35 +0530

    Update NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,12 @@
-import './NavBar.css'
-
+import { Button } from "../Button/Button";
+import "./NavBar.css";
 
 export const NavBar = () => {
-    return <div className='nav-bar'></div>
-}
+  return (
+    <div className="nav-bar">
+      <Button props={{ name: "Home", link: "/" }} />
+      <Button props={{ name: "Add Country", link: "/add-country" }} />
+      <Button props={{ name: "Add City", link: "/add-city" }} />
+    </div>
+  );
+};
