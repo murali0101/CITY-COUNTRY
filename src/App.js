ab5b2782c8bd8a2dcdbd8a3e89a49caf209853b0
murali0101 <muralimv0101@gmail.com>
2022-05-30 20:22:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { AddCity } from './components/AddCity/AddCity';
 import { AddCountry } from './components/AddCountry/AddCountry';
 import { NavBar } from './components/NavBar/NavBar';
 import { Table } from './components/Table/Table';
+
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <AddCountry/>
+      <AddCountry />
+      <AddCity/>
       <Routes>
         <Route path='/' element={<Table/>}>
         
