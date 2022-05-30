695d6b8795fd86efdaf8c5b87b875df8f84ac86e
murali0101 <muralimv0101@gmail.com>
2022-05-30 12:37:42 +0530

    Update Table.jsx

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,29 +1,33 @@
-import './Table.css'
-import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getData } from '../../redux/cityCountryReducer/CityCountryAction'
-import { TableRow } from './TableRow'
+import "./Table.css";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getData } from "../../redux/cityCountryReducer/CityCountryAction";
+import { TableRow } from "./TableRow";
 
 export const Table = () => {
-    const dispatch = useDispatch()
-    const populationData=useSelector((store)=>store.city_country.city)
-    console.log('populationData:', populationData)
-    useEffect(() => {
-        dispatch(getData())
-    },[])
-    return <table>
-        <thead>
-            <tr>
-                <th>S.No</th>
-                <th>Country</th>
-                <th>City</th>
-                <th>Population</th>
-                <th>Edit</th>
-                <th>Delete</th>
-            </tr>
-        </thead>
-        <tbody>
-  {populationData.map((ele,ind)=><TableRow />)}
-        </tbody>
+  const dispatch = useDispatch();
+  const populationData = useSelector((store) => store.city_country.city);
+  console.log("populationData:", populationData);
+  useEffect(() => {
+    dispatch(getData());
+  }, []);
+  return (
+    <table className="table-details">
+      <thead>
+        <tr>
+          <th>S.No</th>
+          <th>Country</th>
+          <th>City</th>
+          <th>Population</th>
+          <th>Edit</th>
+          <th>Delete</th>
+        </tr>
+      </thead>
+      <tbody>
+        {populationData.map((ele, ind) => (
+          <TableRow props={{ ele, ind }} key={ind} />
+        ))}
+      </tbody>
     </table>
-}
+  );
+};
