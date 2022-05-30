d3496e77c62ce7689005226e0584fc5c1a0379a8
murali0101 <muralimv0101@gmail.com>
2022-05-30 20:41:42 +0530

    Update Button.jsx

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,7 @@
-import './Button.css'
+import { useNavigate } from "react-router-dom";
+import "./Button.css";
 
-export const Button = () => {
-    return <button></button>
-}
+export const Button = ({ props }) => {
+  const nav = useNavigate();
+  return <button onClick={() => {nav(props.link)}}>{props.name}</button>;
+};
