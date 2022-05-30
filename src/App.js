b99ec68d0f788a5a8895cd7795432b410d89958c
murali0101 <muralimv0101@gmail.com>
2022-05-30 20:41:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import { AddCity } from './components/AddCity/AddCity';
-import { AddCountry } from './components/AddCountry/AddCountry';
-import { NavBar } from './components/NavBar/NavBar';
-import { Table } from './components/Table/Table';
-
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import { AddCity } from "./components/AddCity/AddCity";
+import { AddCountry } from "./components/AddCountry/AddCountry";
+import { NavBar } from "./components/NavBar/NavBar";
+import { Table } from "./components/Table/Table";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <AddCountry />
-      <AddCity/>
-      <Routes>
-        <Route path='/' element={<Table/>}>
-        
 
+      <Routes>
+        <Route path="/" element={<Table />}>
+          {" "}
+        </Route>
+        <Route path="/add-city" element={<AddCity />}>
+          {" "}
+        </Route>
+        <Route path="/add-country" element={<AddCountry />}>
+          {" "}
         </Route>
       </Routes>
     </div>
