e6de750af62042ff09de73b95ae9cc9baabf4bb5
murali0101 <muralimv0101@gmail.com>
2022-05-31 00:15:45 +0530

    Update TableRow.jsx

--- src/components/Table/TableRow.jsx
+++ src/components/Table/TableRow.jsx
@@ -1,15 +1,20 @@
+import { useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { deletecity } from '../../redux/cityCountryReducer/CityCountryAction'
 import './TableRow.css'
 
 
 
 export const TableRow = ({ props }) => {
-    const {population,countryId:{country_name},city_name}=props.ele
+    const dispatch = useDispatch()
+    const nav=useNavigate()
+    const {population,countryId:{country_name},city_name,_id}=props.ele
     return <tr>
         <td>{props.ind+1 }</td>
         <td>{country_name}</td>
         <td>{city_name}</td>
         <td>{population}</td>
-        <td>Edit</td>
-        <td>Delete</td>
+        <td><button onClick={() => { nav(`/edit-city/${_id}`) }}>Edit</button></td>
+        <td><button onClick={()=>{dispatch(deletecity(_id))}}>Delete</button></td>
     </tr>
 }
