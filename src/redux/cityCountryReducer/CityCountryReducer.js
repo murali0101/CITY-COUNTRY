80501c07ea6aefe0b196f4777a939e10b8c9655d
murali0101 <muralimv0101@gmail.com>
2022-05-30 12:37:15 +0530

    Update CityCountryReducer.js

--- src/redux/cityCountryReducer/CityCountryReducer.js
+++ src/redux/cityCountryReducer/CityCountryReducer.js
@@ -1,9 +1,14 @@
+import { ADDDATA } from "./CityCountryAction";
+
 const init = {
   country: [],
   city: [],
 };
 export const CityCountryReducer = (store = init, { type, payload }) => {
   switch (type) {
+    case ADDDATA:
+      return { ...store, city: payload };
+
     default:
       return store;
   }
