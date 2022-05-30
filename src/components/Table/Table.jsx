3c50616af68e2158af62fc564825508be4a61cf6
murali0101 <muralimv0101@gmail.com>
2022-05-30 22:49:29 +0530

    Update Table.jsx

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,36 +1,43 @@
 import "./Table.css";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getCountry, getData } from "../../redux/cityCountryReducer/CityCountryAction";
+import {
+  getCountry,
+  getData,
+} from "../../redux/cityCountryReducer/CityCountryAction";
 import { TableRow } from "./TableRow";
+import { SortFilter } from "../SortFilter/SortFilter";
 
 export const Table = () => {
   const dispatch = useDispatch();
   const populationData = useSelector((store) => store.city_country.city);
   const countryData = useSelector((store) => store.city_country.country);
-  // console.log("populationData:", populationData);
-  // console.log("countryData:", countryData);
+  console.log("populationData:", populationData);
+  console.log("countryData:", countryData);
   useEffect(() => {
     dispatch(getData());
     dispatch(getCountry());
   }, []);
   return (
-    <table className="table-details">
-      <thead>
-        <tr>
-          <th>S.No</th>
-          <th>Country</th>
-          <th>City</th>
-          <th>Population</th>
-          <th>Edit</th>
-          <th>Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        {populationData.map((ele, ind) => (
-          <TableRow props={{ ele, ind }} key={ind} />
-        ))}
-      </tbody>
-    </table>
+    <div >
+      <SortFilter/>
+      <table className="table-details">
+        <thead>
+          <tr>
+            <th>S.No</th>
+            <th>Country</th>
+            <th>City</th>
+            <th>Population</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {populationData.map((ele, ind) => (
+            <TableRow props={{ ele, ind }} key={ind} />
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
